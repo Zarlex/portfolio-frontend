67ef84a951bcc2456a1804272e24ad8495703c18
Alexander Zarges <alex.zarges@online.de>
2014-11-15 14:31:44 +0100

    made color object a Backbone model

--- app/modules/zx_canvas/src/canvas/color.js
+++ app/modules/zx_canvas/src/canvas/color.js
@@ -2,12 +2,30 @@
  * Created by zarges on 05.11.14.
  */
 
-var Color = function (r,g,b,a) {
-    var _hex = r;
+var Color = zxBackbone.Model.extend({
+    defaults: function () {
+        return {
+            red: 0,
+            green: 0,
+            blue: 0,
+            alpha: 0
+        }
+    },
 
-    this.get = function(){
-        return _hex;
-    };
-};
+    toHexString: function () {
+        return '#' +
+            this.get('red').toString(16) +
+            this.get('green').toString(16) +
+            this.get('blue').toString(16);
+    },
+
+    toRgbString: function () {
+        return 'rgb(' + this.get('red') + ',' + this.get('green') + ',' + this.get('blue') + ')';
+    },
+
+    toRgbaString: function () {
+        return 'rgba(' + this.get('red') + ',' + this.get('green') + ',' + this.get('blue') + ',' + this.get('alpha') + ')';
+    }
+});
 
 zxCanvas.Color = Color;
