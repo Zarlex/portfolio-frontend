9625ad2ad2e5ded99e0c09936df51740611960f2
Alexander Zarges <alex.zarges@online.de>
2014-11-18 22:24:14 +0100

    added canvas attributes model

---
+++ app/modules/zx_canvas/src/canvas/canvas_attributes.js
@@ -0,0 +1,21 @@
+/**
+ * Created by zarges on 18.11.14.
+ */
+
+var CanvasAttributes = zxBackbone.NestedModel.extend({
+    defaults: function () {
+        return {
+            rendering: false,
+            width: 200,
+            height: 200
+        }
+    },
+
+    nested: function(){
+        return {
+            layers: zxCanvas.Layers
+        }
+    }
+});
+
+zxCanvas.CanvasAttributes = CanvasAttributes;
