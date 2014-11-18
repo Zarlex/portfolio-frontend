b145a80ddf12696513ed192ad6dfeb9119b25a69
Alexander Zarges <alex.zarges@online.de>
2014-11-18 22:34:27 +0100

    added loadImage function

--- app/modules/zx_canvas/src/canvas/image_layer.js
+++ app/modules/zx_canvas/src/canvas/image_layer.js
@@ -22,7 +22,7 @@ var ImageLayer = zxCanvas.Layer.extend({
     render: function (img) {
         var context = this.get('canvas').getContext('2d'),
             renderRectangle = this.getRenderRectangle();
-        console.log('loaded img')
+        console.log('loaded img');
         context.clearRect.apply(context, renderRectangle);
         context.drawImage(this.get('imageInstance'), renderRectangle[0], renderRectangle[1], renderRectangle[2], renderRectangle[3]);
     },
@@ -31,28 +31,32 @@ var ImageLayer = zxCanvas.Layer.extend({
         var self = this,
             imageInstance = this.get('imageInstance');
 
-        this.set('rendering',this.get('syncron'));
-        imageInstance.src = this.get('src');
-
         if(this.get('imageLoaded')){
             self.set('rendering',true);
             self.render(this);
             self.set('rendering',false);
-        } else {
-            imageInstance.onload = function(){
-                self.set('imageLoaded',true);
-                self.set('rendering',true);
-                self.render(this);
-                self.set('rendering',false);
-            };
         }
+    },
+
+    _loadImage: function(){
+        var imageInstance = this.get('imageInstance'),
+            self = this;
+
+        if(this.get('syncron')){
+            this.set('rendering',true);
+        }
+        this.set('imageLoaded',false);
+        imageInstance.onload = function(){
+            self.set('imageLoaded',true);
+            self.prepareToRender();
+        };
+        imageInstance.src = this.get('src');
     },
 
     constructor: function () {
         var constructor = zxCanvas.Layer.prototype.constructor.apply(this,arguments);
         this.on('change:src', function () {
-            this.set('imageLoaded',false);
-            this.prepareToRender();
+            this._loadImage();
         }, this);
         return constructor;
     }
