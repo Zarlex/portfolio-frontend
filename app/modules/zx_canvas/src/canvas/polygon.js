3ffa58cb2acafd76c592073df0f138a27c5b7898
Alexander Zarges <alex.zarges@online.de>
2014-12-10 23:07:27 +0100

    removed animationQueue attribute and added postion model

--- app/modules/zx_canvas/src/canvas/polygon.js
+++ app/modules/zx_canvas/src/canvas/polygon.js
@@ -6,28 +6,31 @@ var Polygon = zxBackbone.NestedModel.extend({
 
     nested: function () {
         return {
+            position: zxCanvas.Coordinate,
             coordinates: zxCanvas.Coordinates,
-            color: zxCanvas.Color,
-            animationQueue: zxBackbone.Collection
+            color: zxCanvas.Color
         };
     },
 
     render: function (context) {
         var coordinates = this.get('coordinates').toJSON(),
             startEndPoint = coordinates.splice(0,1)[0],
-            color = this.get('color').toRgbaString();
+            color = this.get('color').toRgbaString(),
+            position = this.get('position'),
+            x = position.get('x'),
+            y = position.get('y');
 
         context.fillStyle = color;
 
         context.beginPath();
 
-        context.moveTo(startEndPoint.x, startEndPoint.y);
+        context.moveTo(startEndPoint.x+x, startEndPoint.y+y);
 
         coordinates.forEach(function (coordinate) {
-            context.lineTo(coordinate.x, coordinate.y);
+            context.lineTo(coordinate.x+x, coordinate.y+y);
         });
 
-        context.lineTo(startEndPoint.x, startEndPoint.y);
+        context.lineTo(startEndPoint.x+x, startEndPoint.y+y);
 
         context.closePath();
         context.fill();
