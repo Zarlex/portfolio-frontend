0aef6fae4d7987dbe16b75139ffff052f316f910
Alexander Zarges <alex.zarges@online.de>
2014-12-12 21:57:17 +0100

    added layerfacotry and changed srcs of layers to new folder

--- app/modules/zx_canvas/src/zx_canvas.js
+++ app/modules/zx_canvas/src/zx_canvas.js
@@ -3,7 +3,12 @@
 
     var zxCanvas = {};
 
-    // @include ./canvas/layers.js
+    // @include ./layer/layer.js
+    // @include ./layer/layers.js
+    // @include ./layer/polygon_layer.js
+    // @include ./layer/image_layer.js
+    // @include ./layer/layer_factory.js
+
     // @include ./canvas/canvas_attributes.js
     // @include ./canvas/canvas.js
     // @include ./canvas/coordinate.js
@@ -11,9 +16,6 @@
     // @include ./canvas/color.js
     // @include ./canvas/polygon.js
     // @include ./canvas/polygons.js
-    // @include ./canvas/layer.js
-    // @include ./canvas/polygon_layer.js
-    // @include ./canvas/image_layer.js
 
     // @include ./animation_queue/animation_object.js
     // @include ./animation_queue/animation_queue_attributes.js
