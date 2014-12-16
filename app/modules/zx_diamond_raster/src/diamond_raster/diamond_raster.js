1fbe1f8e0316ea9abfe58bfe9d5309f6e3e0d55f
Alexander Zarges <alex.zarges@online.de>
2014-12-16 21:32:49 +0100

    Renamed rowows to rowsAmount and columns to ColumnsAmount // implemented getRows and getcolumns

--- app/modules/zx_diamond_raster/src/diamond_raster/diamond_raster.js
+++ app/modules/zx_diamond_raster/src/diamond_raster/diamond_raster.js
@@ -4,52 +4,84 @@
 
 var DiamondRaster = zxCanvas.PolygonLayer.extend({
 
-    defaults : function(){
-        var defaults = zxCanvas.PolygonLayer.prototype.defaults.apply(this,arguments);
-        return _.extend(defaults,{
-            rows: 0,
-            columns: 0,
-            diamondWidth: 0,
-            diamondHeight: 0
-        });
-    },
+            defaults: function () {
+                var defaults = zxCanvas.PolygonLayer.prototype.defaults.apply(this, arguments);
+                return _.extend(defaults, {
+                    rowAmount: 0,
+                    columnAmount: 0,
+                    diamondWidth: 0,
+                    diamondHeight: 0
+                });
+            },
 
-    _generate: function(){
-        var polygons = this.get('polygons'),
-            rows = this.get('rows'),
-            columns = this.get('columns');
+            getRows: function () {
 
-        polygons.reset();
+                var polygons = this.get('polygons'),
+                    rowAmount = this.get('rowAmount'),
+                    columnAmount = this.get('columnAmount'),
+                    rows = [];
 
-        for(var r=0;r<rows;r++){
-            for(var c=0;c<columns;c++){
-                var dWitdh = this.get('diamondWidth'),
-                    dHeight = this.get('diamondHeight'),
-                    diamond = new zxDiamondRaster.Diamond({width:dWitdh,height:dHeight});
+                for (var r = -0.5; r < rowAmount; r += 0.5) {
+                    var row = [];
+                    for (var c = r % 1 > 0 ? 0.5 : 0; c < columnAmount; c += 1) {
+                        var diamond = polygons.findWhere({row:r,column:c});
+                        row.push(diamond);
+                    }
+                    rows.push(row);
+                }
 
-                diamond.get('position').set({x:c*dWitdh,y:r*dHeight});
-                polygons.add(diamond);
+                return rows;
+            },
+
+            getColumns: function(){
+                var polygons = this.get('polygons'),
+                    rowAmount = this.get('rowAmount'),
+                    columnAmount = this.get('columnAmount'),
+                    columns = [];
+
+                for (var c = 0; c < columnAmount; c += 0.5) {
+                    var column = [];
+                    for (var r = -0.5; r < rowAmount; r += 0.5) {
+                        var diamond = polygons.findWhere({row:r,column:c});
+                        if(diamond){
+                            column.push(diamond);
+                        }
+
+                    }
+                    columns.push(column);
+                }
+
+                return columns;
+
+            },
+
+            _generate: function () {
+                var polygons = this.get('polygons'),
+                    rowAmount = this.get('rowAmount'),
+                    columnAmount = this.get('columnAmount');
+
+                polygons.reset();
+
+                for (var r = -0.5; r < rowAmount; r += 0.5) {
+                    for (var c = r % 1 > 0 ? 0.5 : 0; c < columnAmount; c += 1) {
+                        var dWitdh = this.get('diamondWidth'),
+                            dHeight = this.get('diamondHeight'),
+                            diamond = new zxDiamondRaster.Diamond({width: dWitdh, height: dHeight});
+
+                        diamond.get('position').set({x: c * dWitdh, y: r * dHeight});
+                        diamond.set({row:r,column:c});
+                        polygons.add(diamond);
+                    }
+                }
+            },
+
+            constructor: function () {
+                var superConstructor = zxCanvas.PolygonLayer.prototype.constructor.apply(this, arguments);
+                this._generate();
+                return superConstructor;
             }
+
         }
-
-        for(var r2= 0.5;r2<rows;r2++){
-            for(var c2= 0.5;c2<columns;c2++){
-                var dWitdh2 = this.get('diamondWidth'),
-                    dHeight2 = this.get('diamondHeight'),
-                    diamond2 = new zxDiamondRaster.Diamond({width:dWitdh2,height:dHeight2});
-
-                diamond2.get('position').set({x:c2*dWitdh2,y:r2*dHeight2});
-                polygons.add(diamond2);
-            }
-        }
-    },
-
-    constructor: function(){
-        var superConstructor = zxCanvas.PolygonLayer.prototype.constructor.apply(this,arguments);
-        this._generate();
-        return superConstructor;
-    }
-
-});
+    );
 
 zxDiamondRaster.DiamondRaster = DiamondRaster;
