e0db4abc96eaf132dfc327e12215be28f3c80931
Alexander Zarges <alex.zarges@online.de>
2014-12-23 21:45:17 +0100

    updated ui test to test circles

--- app/modules/start/directive/start_directive.js
+++ app/modules/start/directive/start_directive.js
@@ -19,80 +19,25 @@
                             width: angular.element(document).width(),
                             height: angular.element(document).height()
                         },
-                        diamondRasterLayer = layerFactory.createLayer(zxDiamondRaster.DiamondRaster,{rowAmount:5,columnAmount:9,diamondHeight:240,diamondWidth:240,position:{x:-120,y:-120}});
+                        diamondRasterLayer = layerFactory.createLayer(zxDiamondRaster.DiamondRaster,{rowAmount:5,columnAmount:5,diamondHeight:120,diamondWidth:120,position:{x:0,y:0}});
 
-                    canvas.attributes.set({width: windowSize.width+120, height: windowSize.height+120});
+                    canvas.attributes.set({width: windowSize.width, height: windowSize.height});
                     layers.add(diamondRasterLayer);
 
                     scope.$watch('opacity',function(value){
                         if(value){
 
-                            var rows = diamondRasterLayer.getRows(),
-                                columns = diamondRasterLayer.getColumns();
-                            rows[4].forEach(function(diamond){
+                            var circles = diamondRasterLayer.getCircles();
+
+                            circles[0].forEach(function(diamond){
                                 diamond.set({color:{alpha:value/100}});
                             });
-                            rows[6].forEach(function(diamond){
+
+                            circles[2].forEach(function(diamond){
                                 diamond.set({color:{alpha:value/100}});
                             });
-                            columns[4].forEach(function(diamond){
-                                diamond.set({color:{alpha:value/100}});
-                            });
-                            columns[6].forEach(function(diamond){
-                                diamond.set({color:{alpha:value/100}});
-                            });
-                            //diamondRasterLayer.get('polygons').each(function(diamond){
-                            //    diamond.set({color:{alpha:value/100}});
-                            //});
                         }
                     });
-
-
-                    //var diamondRaster = zxDiamondRaster.DiamondRaster();
-                    //canvas.createLayer(diamondRaster,300);
-                    //var layer = canvas.createPolygonLayer(100);
-                    //var layer2 = canvas.createPolygonLayer(200);
-                    //var layer2b = canvas.createPolygonLayer(30);
-                    //var layer3 = canvas.createImageLayer(4);
-                    //var polygon = new zxCanvas.Polygon({coordinates: [
-                    //    {x: 0, y: 0},
-                    //    {x: 300, y: 0},
-                    //    {x: 100, y: 500},
-                    //    {x: 0, y: 200}
-                    //], color: {red: 144, blue: 144, green: 144}});
-                    //var polygon2 = new zxCanvas.Polygon({coordinates: [
-                    //    {x: 200, y: 0},
-                    //    {x: 400, y: 0},
-                    //    {x: 400, y: 200},
-                    //    {x: 200, y: 200}
-                    //], color: {red: 255, blue: 0, green: 255}});
-                    //var polygon3 = new zxCanvas.Polygon({coordinates: [
-                    //    {x: 200, y: 400},
-                    //    {x: 400, y: 400},
-                    //    {x: 400, y: 600},
-                    //    {x: 200, y: 600}
-                    //], color: {red: 255, blue: 0, green: 255}});
-                    //var diamond = new zxDiamondRaster.Diamond({height: 100, width:100,position:{x:0,y:0}});
-                    //
-                    //console.log(diamond);
-                    //
-                    //canvas.attributes.set({width: windowSize.width, height: windowSize.height});
-                    //
-                    //layer.get('polygons').add(polygon);
-                    //layer2.get('polygons').add(polygon2);
-                    //layer2b.get('polygons').add(polygon3);
-                    //layer2.get('polygons').add(diamond);
-                    //
-                    //layer3.set('src', '/modules/start/images/southside.jpg');
-                    //
-                    //window.polygon2 = polygon2;
-                    //layer.set('globalCompositeOperation', 'screen');
-                    //
-                    //setTimeout(function () {
-                    //    zxCanvas.animationQueue.add({duration:2000,model:polygon2.get('color'),attributes:{blue:255,red:0,green:0}});
-                    //    zxCanvas.animationQueue.add({duration:2000,model:polygon3.get('position'),attributes:{x:100,y:100}});
-                    //}, 1000);
-                    //
                 }
             };
         });
