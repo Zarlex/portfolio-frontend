b4619c7430cbe249e66a13c6b12592cf0208a30f
Alexander Zarges <alex.zarges@online.de>
2014-12-25 00:16:57 +0100

    moved diamond directive into portfolo module and updated directive that it centers the raster and creates it in dependency of the screensize

--- app/modules/portfolio/directive/portfolio_directive.js
+++ app/modules/portfolio/directive/portfolio_directive.js
@@ -1,16 +1,79 @@
-(function(){
-  'use strict';
+(function () {
+    'use strict';
 
-  angular.module('Portfolio')
+    angular.module('Portfolio')
 
-    .directive('appendClassAccordingToRoute', function () {
-      return {
-        link: function (scope, el) {
-          scope.$on('$routeChangeSuccess', function (event, current) {
-            el.addClass(current.cssClasses);
-          });
-        }
-      };
-    });
+        .directive('appendClassAccordingToRoute', function () {
+            return {
+                link: function (scope, el) {
+                    scope.$on('$routeChangeSuccess', function (event, current) {
+                        el.addClass(current.cssClasses);
+                    });
+                }
+            };
+        })
+
+        .directive('zxCanvas', function () {
+            return {
+                replace: true,
+                template: '<canvas style="position: absolute; left: 0;top:0"></canvas>', //Width && Height = (row or col amount-1 * diamond width or height)-(diamond width or height)/2
+                scope: {
+                    opacity: '='
+                },
+                link: function (scope, el) {
+
+                    var canvas = new zxCanvas.Canvas({el: el}),
+                        layers = canvas.attributes.get('layers'),
+                        layerFactory = new zxCanvas.LayerFactory(canvas),
+                        windowSize = {
+                            width: angular.element(document).width(),
+                            height: angular.element(document).height()
+                        },
+                        diamondSize = {
+                            width: 240,
+                            height: 240
+                        },
+                        rowAmount = Math.floor(windowSize.height / diamondSize.height) + 2,
+                        columnAmount = Math.floor(windowSize.width / diamondSize.width) + 2,
+                        centerOffset = {
+                            left: (columnAmount * diamondSize.width - windowSize.width) / 2,
+                            top: (rowAmount * diamondSize.height - windowSize.height) / 2
+                        },
+                        diamondRasterLayer = layerFactory.createLayer(
+                            zxDiamondRaster.DiamondRaster, {
+                                rowAmount: rowAmount,
+                                columnAmount: columnAmount,
+                                diamondHeight: diamondSize.height,
+                                diamondWidth: diamondSize.width,
+                                position: {
+                                    x: centerOffset.left * -1,
+                                    y: centerOffset.top * -1
+                                }
+                            }
+                        );
+
+                    canvas.attributes.set({
+                        width: windowSize.width + centerOffset.left,
+                        height: windowSize.height + centerOffset.top
+                    });
+
+                    layers.add(diamondRasterLayer);
+
+                    scope.$watch('opacity', function (value) {
+                        if (value) {
+                            var circles = diamondRasterLayer.getCircles();
+
+                            circles[0].forEach(function (diamond) {
+                                diamond.set({color: {alpha: value / 100}});
+                            });
+
+                            circles[2].forEach(function (diamond) {
+                                diamond.set({color: {alpha: value / 100}});
+                            });
+                        }
+                    });
+                }
+            };
+        });
 
 }());
--- app/modules/start/directive/start_directive.js
+++ app/modules/start/directive/start_directive.js
@@ -1,45 +1,6 @@
 (function () {
     'use strict';
 
-    angular.module('Portfolio')
-
-        .directive('zxCanvas', function () {
-            return {
-                replace: true,
-                template: '<canvas style="position: absolute; left: 0;top:0"></canvas>', //Width && Height = (row or col amount-1 * diamond width or height)-(diamond width or height)/2
-                scope: {
-                    opacity: '='
-                },
-                link: function (scope, el) {
-
-                    var canvas = new zxCanvas.Canvas({el: el}),
-                        layers = canvas.attributes.get('layers'),
-                        layerFactory = new zxCanvas.LayerFactory(canvas),
-                        windowSize = {
-                            width: angular.element(document).width(),
-                            height: angular.element(document).height()
-                        },
-                        diamondRasterLayer = layerFactory.createLayer(zxDiamondRaster.DiamondRaster,{rowAmount:5,columnAmount:5,diamondHeight:120,diamondWidth:120,position:{x:0,y:0}});
-
-                    canvas.attributes.set({width: windowSize.width, height: windowSize.height});
-                    layers.add(diamondRasterLayer);
-
-                    scope.$watch('opacity',function(value){
-                        if(value){
-
-                            var circles = diamondRasterLayer.getCircles();
-
-                            circles[0].forEach(function(diamond){
-                                diamond.set({color:{alpha:value/100}});
-                            });
-
-                            circles[2].forEach(function(diamond){
-                                diamond.set({color:{alpha:value/100}});
-                            });
-                        }
-                    });
-                }
-            };
-        });
+    //angular.module('Portfolio')
 
 }());
