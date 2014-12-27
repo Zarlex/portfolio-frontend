073b30e8b993c5c1e4ad0e8e7f92372c30ea35f0
Alexander Zarges <alex.zarges@online.de>
2014-12-27 22:36:15 +0100

    moved image render and loading functionality into own model

--- app/modules/zx_canvas/src/layer/image_layer.js
+++ app/modules/zx_canvas/src/layer/image_layer.js
@@ -3,74 +3,57 @@
  */
 
 var ImageLayer = zxCanvas.Layer.extend({
-    defaults: function () {
-        var defaults = zxCanvas.Layer.prototype.defaults.apply(this,arguments);
-        return _.extend(defaults,{
-            src: null,
-            syncron: false,
-            imageInstance: new Image(),
-            imageLoaded: false,
-            width: null,
-            height: null
-        });
-    },
 
     nested: function () {
         var nested = zxCanvas.Layer.prototype.nested.apply(this,arguments);
         return _.extend(nested,{
-            position: zxCanvas.Coordinate
+            image: zxCanvas.Image
         });
     },
 
     getRenderRectangle: function () {
-        var img = this.get('imageInstance'),
-            position = this.get('position');
-        return [position.get('x'),position.get('y'),img.width,img.height];
-    },
-
-    render: function () {
-        var context = this.get('canvas').getContext('2d'),
-            renderRectangle = this.getRenderRectangle();
-
-        context.clearRect.apply(context,  this._lastRenderRectangle);
-        context.drawImage(this.get('imageInstance'), renderRectangle[0], renderRectangle[1], renderRectangle[2], renderRectangle[3]);
-        this._lastRenderRectangle = renderRectangle;
+        return this.get('image').getRenderRectangle();
     },
 
     prepareToRender: function(){
-        var self = this;
+        var context = this.get('canvas').getContext('2d'),
+            image = this.get('image'),
+            self = this;
 
-        if(this.get('imageLoaded')){
-            self.set('rendering',true);
-            self.render(this);
-            self.set('rendering',false);
+        if(image.get('imageLoaded') && !this.get('rendering')){
+            var start = +new Date();
+
+            this.set('rendering', true);
+
+            window.requestAnimationFrame(function () {
+                console.log('Start Rendering Layer:',self.get('id'),'IMAGE');
+                self.render(context);
+                console.log('Finished Rendering Layer:',self.get('id'),(+new Date())-start,'IMAGE');
+                self.set('rendering', false);
+            });
         }
     },
 
-    _loadImage: function(){
-        var imageInstance = this.get('imageInstance'),
-            self = this;
+    render: function (context) {
+        var image = this.get('image'),
+            renderRectangle = this.getRenderRectangle();
 
-        if(this.get('syncron')){
-            this.set('rendering',true);
-        }
-        this.set('imageLoaded',false);
-        imageInstance.onload = function(){
-            self.set('imageLoaded',true);
-            self.prepareToRender();
-        };
-        imageInstance.src = this.get('src');
+        context.clearRect.apply(context,  this._lastRenderRectangle);
+
+        image.render(context);
+
+        this._lastRenderRectangle = renderRectangle;
     },
 
     constructor: function () {
-        var constructor = zxCanvas.Layer.prototype.constructor.apply(this,arguments);
-        this.on('change:src', function () {
-            this._loadImage();
-        }, this);
-        this.on('change:position', function(){
+
+        zxCanvas.Layer.prototype.constructor.apply(this,arguments);
+
+        this.on('change:image', function () {
             this.prepareToRender();
-        },this);
-        return constructor;
+        }, this);
+
+        this.get('image').set('context',this.get('canvas').getContext('2d'));
     }
 });
 
