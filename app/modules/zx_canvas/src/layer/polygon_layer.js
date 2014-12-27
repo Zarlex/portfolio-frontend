dba07665fa7ef582fc7597551bd9c07412bf86e7
Alexander Zarges <alex.zarges@online.de>
2014-12-27 22:37:16 +0100

    removed add and remove listeners because they dont make any sense

--- app/modules/zx_canvas/src/layer/polygon_layer.js
+++ app/modules/zx_canvas/src/layer/polygon_layer.js
@@ -15,22 +15,22 @@ var PolygonLayer = zxCanvas.Layer.extend({
         return this.get('polygons').getRenderRectangle();
     },
 
-    render: function () {
-        var context = this.get('canvas').getContext('2d'),
-            renderRectangle = this.getRenderRectangle();
+    render: function (context) {
+        var renderRectangle = this.getRenderRectangle();
 
         context.clearRect.apply(context, this._lastRenderRectangle);
 
         this.get('polygons').each(function (polygon) {
             polygon.render(context);
         }, this);
+
         this._lastRenderRectangle = renderRectangle;
     },
 
     constructor: function () {
 
         zxCanvas.Layer.prototype.constructor.apply(this,arguments);
-        this.on('add remove change:polygons', function () {
+        this.on('change:polygons', function () {
             this.prepareToRender();
         }, this);
     }
