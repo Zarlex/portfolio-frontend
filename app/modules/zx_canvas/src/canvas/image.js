ad667ffe2f298a0af98eeab76fc7bccf688bf0cc
Alexander Zarges <alex.zarges@online.de>
2014-12-27 22:33:12 +0100

    moved image functionality from imagelayer into own model and added dynamic scaling similar to to the image scaling via css

---
+++ app/modules/zx_canvas/src/canvas/image.js
@@ -0,0 +1,94 @@
+/**
+ * Created by zarges on 16.11.14.
+ */
+
+var zxImage = zxBackbone.NestedModel.extend({
+    defaults: function () {
+     return {
+         src: null,
+         syncron: false,
+         imageInstance: new Image(),
+         imageLoaded: false,
+         width: null,
+         height: null,
+         context: null
+     };
+    },
+
+    nested: function () {
+        var nested = zxCanvas.Layer.prototype.nested.apply(this,arguments);
+        return _.extend(nested,{
+            position: zxCanvas.Coordinate
+        });
+    },
+
+    getRenderRectangle: function () {
+        var position = this.get('position'),
+            widthAndHeight = this._getWidthAndHeight(),
+            width = widthAndHeight[0],
+            height = widthAndHeight[1];
+
+        return [position.get('x'),position.get('y'),width, height];
+    },
+
+    render: function (context) {
+        var renderRectangle = this.getRenderRectangle(),
+            imageInstance = this.get('imageInstance');
+
+        if(this.get('imageLoaded')){
+            context.drawImage(imageInstance, renderRectangle[0], renderRectangle[1], renderRectangle[2], renderRectangle[3]);
+        }
+
+    },
+
+    _getWidthAndHeight: function(){
+      var image = this.get('imageInstance'),
+          width = this.get('width'),
+          height = this.get('height'),
+          imageWidth = image.width,
+          imageHeight = image.height,
+          aspectRatio = imageWidth / imageHeight;
+
+        if(width && height){
+            return [width, height];
+        } else if(width){
+            return [width,width/aspectRatio];
+        } else if (height){
+            return [height*aspectRatio,height];
+        } else {
+            return [imageWidth, imageHeight];
+        }
+    },
+
+    _loadImage: function(){
+        var imageInstance = this.get('imageInstance'),
+            self = this;
+
+        if(this.get('syncron')){
+            this.set('rendering',true);
+        }
+        this.set('imageLoaded',false);
+        imageInstance.onload = function(){
+            var context = self.get('context');
+
+            self.set('imageLoaded',true);
+            self.render(context);
+        };
+        imageInstance.src = this.get('src');
+    },
+
+    constructor: function () {
+        var constructor = zxCanvas.Layer.prototype.constructor.apply(this,arguments);
+
+        this.on('change:src', function () {
+            this._loadImage();
+        }, this);
+        if(this.get('src')){
+            this._loadImage();
+        }
+
+        return constructor;
+    }
+});
+
+zxCanvas.Image = zxImage;
