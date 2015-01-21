7c9974176b9aa4d5f2aa0aa9b4fd257ca2140ea5
Alexander Zarges <alex.zarges@online.de>
2015-01-21 23:52:08 +0100

    Changed test to test layerGroups

--- app/modules/portfolio/directive/portfolio_directive.js
+++ app/modules/portfolio/directive/portfolio_directive.js
@@ -16,75 +16,137 @@
         .directive('zxCanvas', function () {
             return {
                 replace: true,
-                template: '<canvas style="position: absolute; left: 0;top:0"></canvas>', //Width && Height = (row or col amount-1 * diamond width or height)-(diamond width or height)/2
+                template: '<canvas style="position: absolute; left: 0;top:0"></canvas>',
                 scope: {
                     opacity: '='
                 },
                 link: function (scope, el) {
 
                     var canvas = new zxCanvas.Canvas({el: el}),
-                        layers = canvas.attributes.get('layers'),
                         layerFactory = new zxCanvas.LayerFactory(canvas),
                         windowSize = {
                             width: angular.element(document).width(),
                             height: angular.element(document).height()
                         },
-                        diamondSize = {
-                            width: 240,
-                            height: 240
-                        },
-                        rowAmount = Math.floor(windowSize.height / diamondSize.height) + 2,
-                        columnAmount = Math.floor(windowSize.width / diamondSize.width) + 2,
-                        centerOffset = {
-                            left: (columnAmount * diamondSize.width - windowSize.width) / 2,
-                            top: (rowAmount * diamondSize.height - windowSize.height) / 2
-                        },
+                        layerGroup = layerFactory.createLayer(zxCanvas.LayerGroup),
+                        diamond1 = layerFactory.createLayer(zxCanvas.PolygonLayer, {polygons:[
+                            new zxDiamondRaster.Diamond({width:100,height:100, color:{red:0}})
+                        ]}),
+                        diamond2 = layerFactory.createLayer(zxCanvas.PolygonLayer, {polygons:[
+                            new zxDiamondRaster.Diamond({width:50,height:50, color:{red:255}})
+                        ]}),
+                        diamond3 = layerFactory.createLayer(zxCanvas.PolygonLayer, {polygons:[
+                            new zxDiamondRaster.Diamond({width:175,height:75, color:{blue:255,alpha:1}})
+                        ]}),
                         imageLayer = layerFactory.createLayer(zxCanvas.ImageLayer, {
                             image: {
                                 src: '/modules/portfolio/images/southside.jpg',
                                 width: windowSize.width
-                            }
-                        }),
-                        diamondRasterLayer = layerFactory.createLayer(
-                            zxDiamondRaster.DiamondRaster, {
-                                rowAmount: rowAmount,
-                                columnAmount: columnAmount,
-                                diamondHeight: diamondSize.height,
-                                diamondWidth: diamondSize.width,
-                                position: {
-                                    x: centerOffset.left * -1,
-                                    y: centerOffset.top * -1
-                                }
-                            }
-                        );
-
-                    canvas.attributes.set({
-                        width: windowSize.width + centerOffset.left,
-                        height: windowSize.height + centerOffset.top
-                    });
-
-                    layers.add(diamondRasterLayer);
-
-                    layers.add(imageLayer);
-
-                    imageLayer.get('image').on('change:imageLoaded', function(){
-                        var circles = diamondRasterLayer.getCircles();
-                        console.log(circles);
-                    });
-
-                    scope.$watch('opacity', function (value) {
-                        if (value) {
-                            var circles = diamondRasterLayer.getCircles();
-
-                            circles[0].forEach(function (diamond) {
-                                diamond.set({color: {alpha: value / 100}});
-                            });
-
-                            circles[2].forEach(function (diamond) {
-                                diamond.set({color: {alpha: value / 100}});
-                            });
                         }
                     });
+
+                    //diamond2.set('globalCompositeOperation', 'copy');
+                    //diamond3.set('globalCompositeOperation', 'destination-out');
+
+                    layerGroup.set('position',{x:100});
+
+                    canvas.setSize(windowSize.width, windowSize.height);
+                    canvas.addLayer(layerGroup);
+
+                    layerGroup.addLayer(diamond1);
+                    layerGroup.addLayer(diamond2);
+
+                    canvas.addLayer(diamond3);
+
+
+                    //var canvas = new zxCanvas.Canvas({el: el}),
+                    //    layers = canvas.attributes.get('layers'),
+                    //    layerFactory = new zxCanvas.LayerFactory(canvas),
+                    //    windowSize = {
+                    //        width: angular.element(document).width(),
+                    //        height: angular.element(document).height()
+                    //    },
+                    //    diamondSize = {
+                    //        width: 240,
+                    //        height: 240
+                    //    },
+                    //    rowAmount = Math.floor(windowSize.height / diamondSize.height) + 2,
+                    //    columnAmount = Math.floor(windowSize.width / diamondSize.width) + 2,
+                    //    centerOffset = {
+                    //        left: (columnAmount * diamondSize.width - windowSize.width) / 2,
+                    //        top: (rowAmount * diamondSize.height - windowSize.height) / 2
+                    //    },
+                    //    imageLayer = layerFactory.createLayer(zxCanvas.ImageLayer, {
+                    //        image: {
+                    //            src: '/modules/portfolio/images/southside.jpg',
+                    //            width: windowSize.width
+                    //        }
+                    //    }),
+                    //    diamondRasterLayer = layerFactory.createLayer(
+                    //        zxDiamondRaster.DiamondRaster, {
+                    //            rowAmount: rowAmount,
+                    //            columnAmount: columnAmount,
+                    //            diamondHeight: diamondSize.height,
+                    //            diamondWidth: diamondSize.width,
+                    //            position: {
+                    //                x: centerOffset.left * -1,
+                    //                y: centerOffset.top * -1
+                    //            }
+                    //        }
+                    //    ),
+                    //    diamondRasterLayer2 = diamondRasterLayer.clone(),
+                    //    diamondRasterCircles = diamondRasterLayer.getCircles(),
+                    //    diamondRasterCircles2 = diamondRasterLayer.getCircles(),
+                    //    diamondRasterCircleStartTimeline = new zxTimeline.Timeline();
+                    //
+                    //var getRandomInt = function (min, max) {
+                    //    return Math.floor(Math.random() * (max - min)) + min;
+                    //};
+                    //
+                    //canvas.attributes.set({
+                    //    width: windowSize.width + centerOffset.left,
+                    //    height: windowSize.height + centerOffset.top
+                    //});
+                    //
+                    //diamondRasterLayer.set('globalCompositeOperation', 'destination-in');
+                    //diamondRasterLayer2.set('globalCompositeOperation', 'screen');
+                    //
+                    //layers.add(diamondRasterLayer);
+                    //
+                    //layers.add(imageLayer);
+                    //
+                    //layers.add(diamondRasterLayer);
+                    //
+                    //layers.add(diamondRasterLayer2);
+
+                    //diamondRasterCircles.reverse().forEach(function (circle, circleIndex) {
+                    //    circle.forEach(function (diamond) {
+                    //        diamond.set('color', {alpha: 0});
+                    //        diamondRasterCircleStartTimeline.add(diamond.get('color'), {alpha: 1}, {
+                    //            duration: 10,
+                    //            offset: 2 * circleIndex
+                    //        });
+                    //    });
+                    //});
+                    //
+                    //
+                    //var diamondRasterCircleStartTimelineDuration = diamondRasterCircleStartTimeline.get('totalFrames');
+                    //
+                    //diamondRasterCircles2.forEach(function (circle, circleIndex) {
+                    //    circle.forEach(function (diamond) {
+                    //        diamond.set('color', {red: 150, blue: 150, green: 150, alpha: 0});
+                    //        diamondRasterCircleStartTimeline.add(diamond.get('color'), {alpha: getRandomInt(20, 80) / 100}, {
+                    //            duration: 40,
+                    //            offset: 2 * circleIndex + diamondRasterCircleStartTimelineDuration
+                    //        });
+                    //    });
+                    //});
+
+                    //imageLayer.get('image').on('change:imageLoaded', function () {
+                    //    var circles = diamondRasterLayer.getCircles();
+                    //    console.log(circles);
+                    //    diamondRasterCircleStartTimeline.play();
+                    //});
                 }
             };
         });
