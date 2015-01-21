9a6dd862e07d503026a000001545527928fb8940
Alexander Zarges <alex.zarges@online.de>
2015-01-21 23:44:04 +0100

    Moved layers functionality into layerGroup Collection

--- app/modules/zx_canvas/src/canvas/canvas.js
+++ app/modules/zx_canvas/src/canvas/canvas.js
@@ -14,96 +14,36 @@ var Canvas = Backbone.View.extend({
         return this.el.getContext('2d');
     },
 
-    getRenderRectangle: function () {
-        var layers = this.attributes.get('layers'),
-            xCoordinates = [0],
-            yCoordinates = [0],
-            width = [],
-            height = [];
-
-        layers.forEach(function (layer) {
-            var renderRectangle = layer.getRenderRectangle();
-
-            if(renderRectangle[0]>0){
-                xCoordinates.push(renderRectangle[0]);
-            }
-
-            if(renderRectangle[1]>0){
-                yCoordinates.push(renderRectangle[1]);
-            }
-
-            width.push(renderRectangle[2] + renderRectangle[0]);
-            height.push(renderRectangle[3] + renderRectangle[1]);
-        });
-
-        xCoordinates = zxCanvas.utils.sortNumbers(xCoordinates);
-        yCoordinates = zxCanvas.utils.sortNumbers(yCoordinates);
-        width = zxCanvas.utils.sortNumbers(width);
-        height = zxCanvas.utils.sortNumbers(height);
-
-        return [xCoordinates[0], yCoordinates[0], width[width.length-1], height[height.length-1]];
+    setCanvasSize: function () {
+        console.log('SET CANVAS')
+        this.el.width = this.attributes.get('width');
+        this.el.height = this.attributes.get('height');
     },
 
-    render: function () {
-        var layers = this.attributes.get('layers'),
-            context = this.getContext(),
-            renderRectangle = this.getRenderRectangle(),
-            startCanvas = +new Date();
+    setSize: function(width, height){
+        console.log('set size',width,height)
+        var layerGroup = this.attributes.get('layerGroup');
 
-        console.log('Start Rendering Canvas', this._lastRenderRectangle);
-        context.clearRect.apply(context, this._lastRenderRectangle);
+        this.attributes.set('width', width);
+        this.attributes.set('height', height);
 
-        layers.forEach(function (layer) {
-            context.save();
-            context.globalCompositeOperation = layer.get('globalCompositeOperation');
-            context.globalAlpha = layer.get('globalAlpha');
-            context.drawImage(layer.get('canvas'), layer.get('position').get('x'), layer.get('position').get('y'));
-            context.restore();
-        }, this);
-
-        this._lastRenderRectangle = renderRectangle;
-        console.log('Finished Rendering Canvas', (+new Date()) - startCanvas);
+        layerGroup.setSize(width, height);
     },
 
-    prepareToRender: function () {
-        var rendering = this.attributes.get('rendering');
-        if (this._layersAreReadyToRender()) {
-            this.render();
-            this.attributes.set('rendering', false);
-        } else if (!rendering) {
-            var self = this;
-            this.attributes.set('rendering', true);
-            window.requestAnimationFrame(function () {
-                self.prepareToRender();
-            });
-        }
-    },
+    addLayer: function(layer){
+        var layerGroup = this.attributes.get('layerGroup');
 
-    _layersAreReadyToRender: function () {
-        var layers = this.attributes.get('layers'),
-            ready = true;
-
-        layers.forEach(function (layer) {
-            if (layer.get('rendering')) {
-                ready = false;
-            }
-        });
-        return ready;
-    },
-
-    _setSize: function () {
-        var width = this.attributes.get('width'),
-            height = this.attributes.get('height');
-
-        this.el.width = width;
-        this.el.height = height;
+        layerGroup.addLayer(layer);
     },
 
     constructor: function () {
-        var constructor = Backbone.View.prototype.constructor.apply(this, arguments);
-        this._setSize();
+        var constructor = Backbone.View.prototype.constructor.apply(this, arguments),
+            layerGroup = this.attributes.get('layerGroup');
 
-        this.attributes.on('change:width change:height', this._setSize, this);
+
+        layerGroup.set('canvas', this.el);
+        this.attributes.set('layerGroup', layerGroup);
+        this.attributes.on('change:width change:height', this.setCanvasSize, this);
         return constructor;
     }
 
