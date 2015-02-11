7b57d483e06c2f0200769a95deb84bf7c59fcc71
Alexander Zarges <alex.zarges@online.de>
2015-02-11 22:09:54 +0100

    canvas is now the central point to control the rendering and compositing to improve performance

--- app/modules/zx_canvas/src/canvas/canvas.js
+++ app/modules/zx_canvas/src/canvas/canvas.js
@@ -19,7 +19,7 @@ var Canvas = Backbone.View.extend({
         this.el.height = this.attributes.get('height');
     },
 
-    setSize: function(width, height){
+    setSize: function (width, height) {
         var layerGroup = this.attributes.get('layerGroup');
 
         this.attributes.set('width', width);
@@ -28,23 +28,62 @@ var Canvas = Backbone.View.extend({
         layerGroup.setSize(width, height);
     },
 
-    addLayer: function(layer){
+    addLayer: function (layer) {
         var layerGroup = this.attributes.get('layerGroup');
 
         layerGroup.addLayer(layer);
     },
 
+    compositeCanvas: function(){
+        var layerGroup = this.attributes.get('layerGroup'),
+            needsCompositing = layerGroup.get('needsCompositing'),
+            isCompositing = layerGroup.get('isCompositing'),
+            canvasIsCompositing = this.attributes.get('canvasIsCompositing');
+
+        if(needsCompositing && !isCompositing && !canvasIsCompositing){
+            this.attributes.set('canvasIsCompositing', true);
+            window.requestAnimationFrame(function () {
+                layerGroup.compositeLayers.call(layerGroup);
+                this.attributes.set('canvasIsCompositing', false);
+            }.bind(this));
+        }
+    },
+
+    renderCanvas: function () {
+        var layerGroup = this.attributes.get('layerGroup'),
+            needsRendering = layerGroup.get('needsRendering'),
+            isRendering = layerGroup.get('isRendering'),
+            canvasIsRendering= this.attributes.get('canvasIsRendering');
+
+        if (needsRendering && !isRendering && !canvasIsRendering) {
+            this.attributes.set('canvasIsRendering', true);
+            window.requestAnimationFrame(function () {
+                this.attributes.set('canvasIsCompositing', true);
+                layerGroup.renderLayers();
+                layerGroup.compositeLayers();
+                this.attributes.set('canvasIsCompositing', false);
+                this.attributes.set('canvasIsRendering', false);
+            }.bind(this));
+        }
+    },
+
     constructor: function () {
-        var constructor = Backbone.View.prototype.constructor.apply(this, arguments),
+        var superConstructor = Backbone.View.prototype.constructor.apply(this, arguments),
             layerGroup = this.attributes.get('layerGroup');
 
-
         layerGroup.set('canvas', this.el);
+        layerGroup.set('name', 'MAINLAYERGROUP');
+
+        layerGroup.on('change:needsRendering', this.renderCanvas, this);
+        layerGroup.on('change:needsCompositing', this.compositeCanvas, this);
+
         this.attributes.set('layerGroup', layerGroup);
         this.attributes.on('change:width change:height', this.setCanvasSize, this);
-        return constructor;
+
+        return superConstructor;
     }
 
 });
 
 zxCanvas.Canvas = Canvas;
+
