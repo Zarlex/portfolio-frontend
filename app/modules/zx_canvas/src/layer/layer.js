0569e8edc83fd76c7df647b62eb9eb93d03f794d
Alexander Zarges <alex.zarges@online.de>
2015-02-15 21:58:19 +0100

    added postRender procedure

--- app/modules/zx_canvas/src/layer/layer.js
+++ app/modules/zx_canvas/src/layer/layer.js
@@ -9,15 +9,21 @@ var Layer = zxBackbone.NestedModel.extend({
             globalCompositeOperation: 'source-over',
             canvas: null,
             isRendering: false,
+            isPostRendering: false,
+            needsRendering: false,
+            needsPostRendering: false,
             globalAlpha: 1,
             width: 0,
-            height: 0
+            height: 0,
+            blur: 0,
+            bAndW: false
         };
     },
 
     nested: function () {
         return {
-            position: zxCanvas.Coordinate
+            position: zxCanvas.Coordinate,
+            effects: zxBackbone.Collection
         };
     },
 
@@ -33,6 +39,27 @@ var Layer = zxBackbone.NestedModel.extend({
 
     },
 
+    postRenderLayer: function () {
+        var isRendering = this.get('isRendering'),
+            needsPostRendering = this.get('needsPostRendering'),
+            isPostRendering = this.get('isPostRendering'),
+            effects = this.get('effects'),
+            context = this.get('canvas').getContext('2d'),
+            width = this.get('width'),
+            height = this.get('height'),
+            position = this.get('position');
+
+        if (!isRendering && needsPostRendering && !isPostRendering) {
+            this.set('isPostRendering', true);
+            console.log('%c' + this.get('name') + ' is postrendering', 'color:lila; font-weight:bold');
+            effects.each(function (effect) {
+                effect.apply(context, position.get('x'), position.get('y'), width, height);
+            });
+            this.set('isPostRendering', false);
+            this.set('needsPostRendering', false);
+        }
+    },
+
     setSize: function (width, height) {
         this.set('width', width);
         this.set('height', height);
@@ -45,14 +72,20 @@ var Layer = zxBackbone.NestedModel.extend({
     },
 
     askForRendering: function () {
-        console.log('%c' + this.get('name') + ' needs rendering', 'color:orange; font-weight:bold');
         this.set('needsRendering', true);
     },
+
+    askForPostRendering: function () {
+        this.set('needsPostRendering', true);
+    },
+
 
     constructor: function () {
         var constructor = zxBackbone.NestedModel.prototype.constructor.apply(this, arguments);
 
         this.on('change:width change:height', this.setCanvasSize, this);
+
+        this.on('change:isRendering', this.askForPostRendering);
 
         return constructor;
     }
