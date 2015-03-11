37161f9720fc841b09b74d3f8b7eede3cceef99b
Alexander Zarges <alex.zarges@online.de>
2015-03-11 21:32:29 +0100

    changes path of include files and adds filter files

--- app/modules/zx_canvas/src/zx_canvas.js
+++ app/modules/zx_canvas/src/zx_canvas.js
@@ -8,17 +8,24 @@
 
     // @include ./layer/layer.js
     // @include ./layer/layers.js
-    // @include ./layer/polygon_layer.js
-    // @include ./layer/image_layer.js
     // @include ./layer/layer_factory.js
     // @include ./layer/layer_group.js
 
     // @include ./canvas/color.js
     // @include ./canvas/coordinate.js
     // @include ./canvas/coordinates.js
-    // @include ./canvas/image.js
-    // @include ./canvas/polygon.js
-    // @include ./canvas/polygons.js
+
+    // @include ./image/image.js
+    // @include ./image/image_layer.js
+
+    // @include ./polygon/polygon.js
+    // @include ./polygon/polygons.js
+    // @include ./polygon/polygon_layer.js
+
+    // @include ./filter/filter.js
+    // @include ./filter/filters.js
+    // @include ./filter/black_and_white_filter.js
+    // @include ./filter/blur_filter.js
 
     // @include ./canvas/canvas_attributes.js
     // @include ./canvas/canvas.js
